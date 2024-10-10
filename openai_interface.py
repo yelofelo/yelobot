8611ba0bc0ca7a6b1fdc52b7b03fa86664bd32ad
yelofelo <datyellowfellow@gmail.com>
2024-10-10 00:16:58 -0700

    Fix OpenAIInterface

--- openai_interface.py
+++ openai_interface.py
@@ -1,5 +1,6 @@
 from openai import AsyncOpenAI
 import dotenv
+import asyncio
 
 MODEL = 'ft:gpt-3.5-turbo-0613:personal::7qZhaoF6'
 
@@ -9,13 +10,14 @@ class OpenAIInterface:
         self.model = model
 
     async def generate(self, prompt, system_message, stop='\n\n', max_tokens=150, temperature=1.2):
-        return await self.client.chat.completions.create(
+        completions = await self.client.chat.completions.create(
             model=self.model,
             messages=[{'role': 'system', 'content': system_message}, {'role': 'user', 'content': prompt}],
             max_tokens=max_tokens,
             stop=stop,
             temperature=temperature
-        ).choices[0].message['content'].strip()
+        )
+        return completions.choices[0].message.content.strip()
 
 if __name__ == '__main__':
     import os
@@ -23,4 +25,4 @@ if __name__ == '__main__':
     prompt = '''
 YOU CAN ENTER A PROMPT HERE TO TEST LOCALLY
 '''
-    print(OpenAIInterface(os.getenv('OPENAI_API_KEY')).generate(prompt))
+    print(asyncio.run(OpenAIInterface(os.getenv('OPENAI_API_KEY')).generate(prompt, "YeloBot is not an assistant, but a chatbot who tries to fit in with the other members of the chat.")))
